dd621e7e65948b92a92abec1d4c5fd45593c15db
a-bakos <bakosattila@fastem.com>
2016-09-19 17:41:51 +0100

    trivia extractor script

---
+++ trivia_extractor.php
@@ -0,0 +1,28 @@
+        <?php
+            # trivia extractor -- abakos
+
+            # set title number
+            $tt_number = "tt3385516";
+            # read the file/url
+            $file = file_get_contents("http://www.imdb.com/title/$tt_number/trivia");
+            # find trivia
+            # trivia looks like this:
+            # <... class="sodatext">actual trivia here  </div> -- trivia followed by 2 spaces
+            # in the regex pattern: ?<= will match trivia that is preceded by: sodatext">
+            # modifiers: siU
+            # s => makes the period to match newlines [by default it doesn't]
+            # i => case-insensitive search
+            # U => non-greedy match
+            $ext = preg_match_all('!(?<=sodatext">).*  </div>!siU', $file, $matches);
+
+            # loop through the matches array and trim the unnecessary whitespaces
+            foreach ($matches[0] as $key => $value) {
+                $matches[0][$key] = trim($value);
+            }
+
+            # print the array elements / trivia
+            # print_r(expression, return boolean)
+            # when this bool parameter is set to TRUE,
+            # print_r() will return the information rather than print it
+            echo "<pre>" . print_r($matches, 1) . "</pre>";
+        ?>
